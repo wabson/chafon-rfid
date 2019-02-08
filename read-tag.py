e5fc735372342fac7d5cdb82ab5bf50f9cec5f03
Will Abson <will.abson@gmail.com>
2019-02-08 16:44:44 +0000

    Refactor read-tag.py to use new API

--- read-tag.py
+++ read-tag.py
@@ -8,50 +8,47 @@ import sys
 
 from datetime import datetime
 
+from reader.base import ReaderCommand
+from reader.command import G2_TAG_INVENTORY
+from reader.transport import TcpTransport
 from reader.uhfreader18 import G2InventoryResponseFrame
 from sheets import GoogleSheetAppender
 
 TCP_PORT = 6000
-BUFFER_SIZE = 1024
-READ_MESSAGE = bytearray([ 0x04, 0x00, 0x01, 0xdb, 0x4b ])
 
 valid_chars = string.digits + string.ascii_letters
 
-def is_marathon_tag(tag_data):
+def is_marathon_tag(tag):
+    tag_data = tag.epc
     return len(tag_data) == 4 and all([ chr(tag_byte) in valid_chars for tag_byte in tag_data.lstrip('\0') ])
 
 def read_tags(reader_addr, appender):
 
+    get_inventory_uhfreader18 = ReaderCommand(G2_TAG_INVENTORY)
+    transport = TcpTransport(reader_addr=reader_addr, reader_port=TCP_PORT)
     running = True
     while running:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5)
-        try:
-            s.connect((reader_addr, TCP_PORT))
-        except socket.error as err:
-            print 'Unable to connect to reader'
-            continue
         start = time.time()
         try:
             now = datetime.now().time()
-            s.sendall(READ_MESSAGE)
-            data = bytearray(s.recv(BUFFER_SIZE))
-            resp = G2InventoryResponseFrame(data)
+            transport.write(get_inventory_uhfreader18.serialize())
+            resp = G2InventoryResponseFrame(transport.read_frame())
             for tag in resp.get_tag():
                 if (is_marathon_tag(tag)):
-                    boat_num = str(tag.lstrip('\0'))
+                    boat_num = str(tag.epc.lstrip('\0'))
                     boat_time = str(now)[:12]
                     print '{0} {1}'.format(boat_num, boat_time)
                     if appender is not None:
                         appender.add_row([ boat_num, boat_time, '', '' ])
                 else:
-                    print "Non-marathon tag 0x%s" % (binascii.hexlify(tag))
+                    print "Non-marathon tag 0x%s" % (binascii.hexlify(tag.epc))
             #print "received %s tags" % (resp.num_tags)
         except KeyboardInterrupt:
             running = False
             print "KeyboardInterrupt"
         except socket.error as err:
-            pass
+            print 'Unable to connect to reader'
+            continue
         end = time.time()
         s.close()
         #print "elapsed time %.2f" % (end - start)
