3752a6686592cf865eebf690261a751ccea75b1f
Will Abson <will.abson@gmail.com>
2020-10-04 20:59:40 +0100

    Support auto-detection of reader type

--- examples/continuous-read.py
+++ examples/continuous-read.py
@@ -7,16 +7,19 @@ import sys
 
 from datetime import datetime
 
-from chafon_rfid.base import ReaderCommand, ReaderResponseFrame
-from chafon_rfid.command import G2_TAG_INVENTORY, CF_SET_RF_POWER, CF_SET_WORK_MODE_18, CF_SET_WORK_MODE_288M
+from chafon_rfid.base import CommandRunner, ReaderCommand, ReaderInfoFrame, ReaderResponseFrame, ReaderType
+from chafon_rfid.command import G2_TAG_INVENTORY, CF_GET_READER_INFO, CF_SET_RF_POWER, CF_SET_WORK_MODE_18, CF_SET_WORK_MODE_288M
 from chafon_rfid.response import G2_TAG_INVENTORY_STATUS_MORE_FRAMES
 from chafon_rfid.transport import TcpTransport
-from chafon_rfid.uhfreader18 import G2InventoryResponseFrame
+from chafon_rfid.transport_serial import SerialTransport
+from chafon_rfid.uhfreader18 import G2InventoryResponseFrame as G2InventoryResponseFrame18
+from chafon_rfid.uhfreader288m import G2InventoryCommand, G2InventoryResponseFrame
 
 TCP_PORT = 6000
 DELAY = 0.00
 
 valid_chars = string.digits + string.ascii_letters
+
 
 def is_marathon_tag(tag):
     tag_data = tag.epc
@@ -30,20 +33,45 @@ def set_power(transport, power_db):
     return status
 
 
+def get_reader_type(runner):
+    get_reader_info = ReaderCommand(CF_GET_READER_INFO)
+    reader_info = ReaderInfoFrame(runner.run(get_reader_info))
+    return reader_info.type
+
+
 def read_tags(reader_addr, appender):
 
-    get_inventory_uhfreader18 = ReaderCommand(G2_TAG_INVENTORY)
-    transport = TcpTransport(reader_addr=reader_addr, reader_port=TCP_PORT)
-    set_power(transport, 30)
+    # transport = TcpTransport(reader_addr=reader_addr, reader_port=TCP_PORT)
+    # transport = SerialTransport(device='/dev/ttyS0')
+    # transport = SerialTransport(device='/dev/ttyAMA0')
+    # transport = SerialTransport(device='/dev/ttyUSB0')
+    if reader_addr.startswith('/') or reader_addr.startswith('COM'):
+        transport = SerialTransport(device=reader_addr)
+    else:
+        transport = TcpTransport(reader_addr)
+    set_power(transport, 27)
+
+    runner = CommandRunner(transport)
+    reader_type = get_reader_type(runner)
+    if reader_type == ReaderType.UHFReader18:
+        get_inventory_cmd = ReaderCommand(G2_TAG_INVENTORY)
+        frame_type = G2InventoryResponseFrame18
+    elif reader_type == ReaderType.UHFReader288M:
+        get_inventory_cmd = G2InventoryCommand(q_value=4, antenna=0x80)
+        frame_type = G2InventoryResponseFrame
+    else:
+        print('Unknown reader type {}'.format(reader_type))
+        sys.exit(1)
+
     running = True
     while running:
         start = time.time()
         try:
             now = datetime.now().time()
-            transport.write(get_inventory_uhfreader18.serialize())
+            transport.write(get_inventory_cmd.serialize())
             inventory_status = None
             while inventory_status is None or inventory_status == G2_TAG_INVENTORY_STATUS_MORE_FRAMES:
-                resp = G2InventoryResponseFrame(transport.read_frame())
+                resp = frame_type(transport.read_frame())
                 inventory_status = resp.result_status
                 for tag in resp.get_tag():
                     if is_marathon_tag(tag):
@@ -51,7 +79,7 @@ def read_tags(reader_addr, appender):
                         boat_time = str(now)[:12]
                         print('{0} {1}'.format(boat_num, boat_time))
                         if appender is not None:
-                            appender.add_row([ boat_num, boat_time, '', '' ])
+                            appender.add_row([boat_num, boat_time, '', ''])
                     else:
                         print("Non-marathon tag 0x%s" % (tag.epc.hex()))
                 #print "received %s tags" % (resp.num_tags)
@@ -69,6 +97,7 @@ def read_tags(reader_addr, appender):
             running = False
             print("KeyboardInterrupt")
     transport.close()
+
 
 if __name__ == "__main__":
 
