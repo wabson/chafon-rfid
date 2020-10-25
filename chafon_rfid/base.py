2a80fb29543b71fcc2e4a57a4cdf3e1418e18854
Will Abson <will.abson@gmail.com>
2020-10-25 20:57:02 +0000

    Add additional reader types (untested)

--- chafon_rfid/base.py
+++ chafon_rfid/base.py
@@ -77,10 +77,13 @@ class ReaderFrequencyBand(Enum):
 
 class ReaderType(Enum):
 
+    RRU9803M      = 0x03 # CF-RU5102 (desktop USB reader/writer)
     UHFReader18   = 0x09
     UHFReader288M = 0x0c
     UHFReader86   = 0x0f # CF-MU903/CF-MU904 (as documented)
     UHFReader86_1 = 0x10 # CF-MU903/CF-MU904 (actual)
+    RRU9883M      = 0x16 # CF-MU902
+    UHFReader288MP = 0x20 # CF-MU804
 
 
 class ReaderInfoFrame(ReaderResponseFrame):
